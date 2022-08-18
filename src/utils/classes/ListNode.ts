d02bc4c647e5741c2fe61b20837026cb8aa6529c
Ali Bektash <agokselb@gmail.com>
2022-08-18 20:54:47 +0900

    More stable version
    I think it's better to check the type also.

--- src/utils/classes/ListNode.ts
+++ src/utils/classes/ListNode.ts
@@ -2,7 +2,7 @@ class ListNode {
   val: number;
   next: ListNode | null;
   constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
+      this.val = val !== undefined && typeof val === 'number' ? val : 0;
+      this.next = next !== undefined && typeof ListNode ? next : null;
   }
 }
