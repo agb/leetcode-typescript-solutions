caa37d8f38cfbe20fc0c603f4d030cb4c5637517
Ali Bektash <agokselb@gmail.com>
2022-09-18 20:58:26 +0900

    Create 6.ZigzagConversion.ts

---
+++ src/problems/6.ZigzagConversion.ts
@@ -0,0 +1,17 @@
+function convert(s: string, numRows: number): string {
+    if (s.length < numRows || s.length < 2 || numRows < 2) {
+      return s;
+    }
+  
+    const result = new Array(numRows).fill('');
+    const cycle = (numRows - 1) * 2;
+  
+    s.split('').forEach((value, index) => {
+      const rest = index % cycle;
+  
+      const arrayIndex = rest < numRows ? rest : cycle - rest;
+      result[arrayIndex] += value;
+    });
+  
+    return result.join('');
+  }
